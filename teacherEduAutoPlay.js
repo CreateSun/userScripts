1b73ff66dece794e7f449f0549e4935152efbe43
啦啦啦种太阳 <47080570+CreateSun@users.noreply.github.com>
2024-01-18 18:11:08 +0800

    Update teacherEduAutoPlay.js

--- teacherEduAutoPlay.js
+++ teacherEduAutoPlay.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-01-15
 // @description  支持https://course.teacheredu.cn/这个地址下的视频自动播放
-// @author       You
+// @author       createsun
 // @match        https://course.teacheredu.cn/jsw/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=teacheredu.cn
 // @grant        none
